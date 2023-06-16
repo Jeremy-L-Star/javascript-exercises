66c703330309d137d4b50d6bcb778770cd00ac98
Jeremy Lee <135320415+Jeremy-L-Star@users.noreply.github.com>
2023-06-16 17:30:38 +0800

    Add single-value removeFromArray()

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,26 @@
-const removeFromArray = function() {
+// first version, just remove a single value
 
+
+const removeFromArray = function(ary, removeValue) {
+    let removeValueIndex = ary.findIndex((n) => n === removeValue);
+    ary.splice(removeValueIndex, 1);
+    return ary;
 };
+
+// later version: using spread operator, filter() the input Array `ary`.
+// We want to run filter with this callback fn:
+/*
+function notInArgs(value, args) {
+    return !args.includes(value);
+}
+
+const removeFromArray(ary, ...argsToRemove) {
+    return ary.filter(notInArgs)
+}
+
+Will be filled out and tested next week
+*/
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
