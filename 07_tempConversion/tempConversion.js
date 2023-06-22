b2708a7878d47881f35ff1c1b821985e81efc7e7
Jeremy Lee <135320415+Jeremy-L-Star@users.noreply.github.com>
2023-06-22 16:55:53 +0800

    Add finished convertToCelsius function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,4 +1,10 @@
-const convertToCelsius = function() {
+
+// See Wikipedia - Celsius = "Melting and boiling points of water" for 
+// conversion formulae.
+
+const convertToCelsius = function(temp) {
+  const converted = (temp - 32) * (5 / 9);
+  return Math.round(converted * 10) / 10; 
 };
 
 const convertToFahrenheit = function() {
