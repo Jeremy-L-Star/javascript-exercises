8b57eb45495ce9eb570aa5b78cd0243f58084b49
Jeremy Lee <135320415+Jeremy-L-Star@users.noreply.github.com>
2023-06-22 13:17:29 +0800

    Make error check code more readable

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,12 +3,9 @@ const sumAll = function(first, second) {
     let end = second;
     let sum = 0;
 
-    if(typeof first !== 'number' || typeof second !== 'number' ||
-    first < 0 || second < 0) {
-      return 'ERROR';
-  }
+    if(typeof first !== 'number' || typeof second !== 'number') return 'ERROR';
+    if(first < 0 || second < 0) return 'ERROR';
 
-    // may be able to rewrite this with destructuring?
     if (first > second) {
         start = second;
         end = first;
