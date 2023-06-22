cdeb208a13869bb230cb9c33269d81bfe8f50722
Jeremy Lee <135320415+Jeremy-L-Star@users.noreply.github.com>
2023-06-22 17:22:44 +0800

    Add completed convertToFahrenheit function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -7,7 +7,9 @@ const convertToCelsius = function(temp) {
   return Math.round(converted * 10) / 10; 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const converted = (temp * (9 / 5)) + 32;
+  return Math.round(converted * 10) / 10; 
 };
 
 // Do not edit below this line
