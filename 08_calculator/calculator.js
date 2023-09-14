0e8c8d556faed05b46f9ab4cd0a4f4937848cec3
Jeremy Lee <135320415+Jeremy-L-Star@users.noreply.github.com>
2023-09-14 19:11:53 +0800

    Add working factorial function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -23,18 +23,23 @@ const multiply = function(...args) {
 const power = function(value, power) {
   let total = value;
   // first 'iteration' is setting the initial value of the total.
-  for(let i = 1; i <= (power - 1); i++) {
+  for (let i = 1; i <= (power - 1); i++) {
     total *= value;
-    console.log(total);
   }
   return total;
 	// multiply x by itself (y - 1) times
   // eg 4^3 = 64, 4 * 4 * 4 : 4 multiplied by itself (3-1) times.
 };
 
-const factorial = function() {
-	
+const factorial = function(num){
+  if (num === 0) return 1;
+  return factorialHelper(1, num);
 };
+
+const factorialHelper = function(total, currentValue) {
+    if (currentValue === 1) return total;
+    return factorialHelper((total * currentValue), currentValue - 1);
+}
 
 // Do not edit below this line
 module.exports = {
