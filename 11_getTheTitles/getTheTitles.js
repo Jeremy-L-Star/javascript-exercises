d33b8dfe9f6938aa66cb80c0ff30c4742c3e8ad9
Jeremy Lee <135320415+Jeremy-L-Star@users.noreply.github.com>
2023-09-22 18:07:34 +0800

    Add finished getTheTitles function

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,14 @@
-const getTheTitles = function() {
 
+/**
+ * Get the titles from an array of book Objects.
+ * @param {Array} bookArr an Array of Objects
+ */
+const getTheTitles = function(bookArr) {
+    let titles = []
+    bookArr.forEach(book => {
+        titles.push(book.title);
+    });
+    return titles;
 };
 
 // Do not edit below this line
