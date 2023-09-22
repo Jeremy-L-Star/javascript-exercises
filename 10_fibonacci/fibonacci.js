6f0f7f18ef2074af136e3706beba603a5f965fe3
Jeremy Lee <135320415+Jeremy-L-Star@users.noreply.github.com>
2023-09-22 16:38:24 +0800

    Allow fibonacci to accept strings + block negatives

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,16 +1,23 @@
 // 1,1,2,3,5,8,13,21,34,55,89,144,233...
-// the spec omits 0, sice they consider the 6th fibonacci number to be 8.
+// the spec omits 0, since they consider the 6th fibonacci number to be 8.
 
 // 1st num is just 1. // 2nd num is also just 1
 // 3rd = 1st + 2nd. 4th = 3rd + 2nd. 5th = 4th + 3rd, etc
 
 /**
  * Return the nth Fibonacci number.
- * @param {Number} n
+ * Note that the spec doesn't count 0 as a fibonacci number.
+ * If it did, the 6th fibonacci number would be 5 (NOT 8).
+ * I have added 0 to the base case to prevent a stack overflow,
+ * even though it will never be used.
+ * @param {Number | String} input
  * return Number the n-th Fibonacci number.
  */
-const fibonacci = function (n) {
-    if (n === 1 || n === 2) return 1;
+const fibonacci = function (input) {
+    const n = Number(input);
+
+    if (n <= -1) return('OOPS');
+    if (n === 0 || n === 1 || n === 2) return 1;
     return fibonacci(n - 1) + fibonacci(n - 2);
 };
 
