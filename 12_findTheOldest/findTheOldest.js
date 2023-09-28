0d1d24cd1852584bacb53cdd79ac699541156fb6
Jeremy Lee <135320415+Jeremy-L-Star@users.noreply.github.com>
2023-09-28 17:30:40 +0800

    Add ability to calculate oldest if someone's still alive

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,3 +1,15 @@
+/**
+ * Set the age of a person who is still alive - no death date.
+ * @param {Number} yearOfBirth 
+ * @returns Number the age of the person.
+ */
+function setAgeOfLivingPerson(yearOfBirth) {
+    let currentDateTimestamp = Date.now();
+    let currentDate = new Date(currentDateTimestamp);
+    let currentYear = currentDate.getFullYear(); // currently this = 2023.
+
+    return currentYear - yearOfBirth; 
+}
 
 /**
  * Return the oldest Person in an array of people.
@@ -6,10 +18,21 @@
  */
 const findTheOldest = function(people) {
     let oldest = people[0];
-    let curOldestPersonAge = oldest.yearOfDeath - oldest.yearOfBirth;
+    let curOldestPersonAge = oldest?.yearOfDeath - oldest.yearOfBirth;
+
+    // if curOldestPersonAge is NaN, they're still alive
+    // so use the current date to calculate their age.
+    // Note: Optional chaining should be undefined when yearOfDeath doesn't exist. undefined - Number = NaN. Number.NaN() is the best solution here.
+    if(Number.isNaN(curOldestPersonAge)) {
+        curOldestPersonAge = setAgeOfLivingPerson(oldest.yearOfBirth);
+    }
 
     people.forEach((person, index) => {
-        let curPersonAge = person.yearOfDeath - person.yearOfBirth;
+        let curPersonAge = person?.yearOfDeath - person.yearOfBirth;
+
+        if(Number.isNaN(curPersonAge)) {
+            curPersonAge = setAgeOfLivingPerson(person.yearOfBirth);
+        }
         console.log(`current person name: ${person.name}, array index: ${index}`);
 
         if(curPersonAge > curOldestPersonAge) {
